59cc5f42c58779655db6ca163d968b63340f5019
Kemal Fathulla <kemalfathulla@gmail.com>
2020-08-28 20:26:30 +0200

    no message

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import React from 'react'
 import styled from "styled-components/native"
 import Printer from "./src/printer/printer"
 
-
 const View = styled.View`
     flex:1;
     align-items: center;
@@ -30,31 +29,57 @@ const ButtonTitle = styled.Text`
     font-size: 28px;
 `
 
-function Button({title,onPress}){
+const source = { uri: 'https://pos-system-templates.s3.amazonaws.com/test10.pdf', cache: true };
+
+function Button({ title, onPress }) {
 
     return <StyledButton onPress={onPress}>
         <ButtonTitle>{title}</ButtonTitle>
     </StyledButton>
 }
 
+const getPDF = async () => {
+
+    let response = await fetch(
+        `https://pos-system-templates.s3.amazonaws.com/test10.pdf`, {
+        method: 'GET',
+
+        responseType: 'base64'
+    });
+
+    //Create a Blob from the PDF Stream
+    const file = new Blob(
+        [response.data],
+        { type: 'application/pdf' });
+    console.log(file)
+
+}
+
+
 function App() {
 
     function onPress() {
         console.log("button pressed")
-        let html = "Hello world"
-        Printer.printHtml(html)
-            .then(response=> {
-                console.log("response",response)
-            })
-            .catch(error => {
-                console.error(error)
-            })
+        let htmlFull = "<html><body><p>Hello World</p></body></html>"
+        let html = "Hello World"
+        getPDF()
+
+
+        // Printer.printPdf()
+        //     .then(response => {
+        //         console.log("response", response)
+        //     })
+        //     .catch(error => {
+        //         console.error(error)
+        //     })
     }
 
     return <View>
-        <Button title={"Click me"} onPress={onPress}/>
+        <Button title={"Click me"} onPress={onPress} />
     </View>
 }
+
+
 
 
 export default App;
